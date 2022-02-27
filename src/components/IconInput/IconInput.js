007f162e0ee015f0bfd3a8b60d3af9c787445173
Steve Hurd <660233+stevehurd@users.noreply.github.com>
2022-02-27 16:17:07 -0700

    Components built

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,83 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import { AtSign } from "react-feather";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+	small: {
+		fontSize: 14,
+		iconSize: 16,
+		borderThickness: 1,
+		height: 24,
+	},
+	large: {
+		fontSize: 18,
+		iconSize: 24,
+		borderThickness: 2,
+		height: 36,
+	},
 };
+
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+	const styles = STYLES[size];
+
+	return (
+		<InputWrapper style={{}}>
+			<VisuallyHidden>{label}</VisuallyHidden>
+			<IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+				<Icon id={icon} size={styles.iconSize} />
+			</IconWrapper>
+			<NativeInput
+				{...delegated}
+				style={{
+					"--width": width + "px",
+					"--height": styles.height + "px",
+					"--border": styles.borderThickness + "px",
+					"--font-size": styles.fontSize + "px",
+				}}
+			/>
+		</InputWrapper>
+	);
+};
+
+const InputWrapper = styled.div`
+	position: relative;
+	color: ${COLORS.gray700};
+
+	&:hover {
+		color: ${COLORS.black};
+	}
+`;
+
+const IconWrapper = styled.div`
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	margin: auto;
+	width: var(--size);
+	height: var(--size);
+	color: inherit;
+`;
+
+const NativeInput = styled.input`
+	height: var(--height);
+	width: var(--width);
+	padding-left: var(--height);
+	font-size: var(--font-size);
+	border: none;
+	border-bottom: var(--border) solid ${COLORS.transparentGray30};
+	color: inherit;
+	font-weight: 700;
+	outline-offset: 2px;
+
+	&::placeholder {
+		color: ${COLORS.gray500};
+		font-weight: 400;
+	}
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,73 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+	const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+	return (
+		<Wrapper>
+			<NativeSelect value={value} onChange={onChange}>
+				{children}
+			</NativeSelect>
+			<PresentationBit>
+				{displayedValue}
+				<IconWrapper style={{ "--size": 16 + "px" }}>
+					<Icon id="chevron-down" strokeWidth={1} size={16}></Icon>
+				</IconWrapper>
+			</PresentationBit>
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	position: relative;
+	width: max-content;
+`;
+
+const NativeSelect = styled.select`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	opacity: 0;
+	/* Allow the select to span the full height in Safari */
+	-webkit-appearance: none;
+`;
+
+const PresentationBit = styled.div`
+	background-color: ${COLORS.transparentGray15};
+	border-radius: 8px;
+	padding: 12px 16px;
+	padding-right: 52px;
+	font-family: "roboto" sans-serif;
+	font-weight: 400;
+	font-size: 1rem;
+	color: ${COLORS.gray700};
+
+	${NativeSelect}:focus + & {
+		outline: 1px dotted #212121;
+		outline: 5px auto -webkit-focus-ring-color;
+	}
+	${NativeSelect}:hover + & {
+		color: ${COLORS.black};
+	}
+`;
+
+const IconWrapper = styled.div`
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	right: 8px;
+	margin: auto;
+	color: inherit;
+	width: var(--size);
+	height: var(--size);
+	pointer-events: none;
+`;
 
 export default Select;
